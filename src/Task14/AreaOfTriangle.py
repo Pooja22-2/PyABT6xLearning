759ddd9d1bf952bfc5514bfb387a7352cdf9b290
Pooja <poojairam.22@gmail.com>
2025-10-15 23:39:04 +0530

    14thTask

---
+++ src/Task14/AreaOfTriangle.py
@@ -0,0 +1,5 @@
+#Write a python program to calculate the area of a triangle using the formula
+base=10
+height=5
+area=1/2*base*height
+print("Area of the triangle:",area)
---
+++ src/Task14/Check_GreaterThan100.py
@@ -0,0 +1,6 @@
+#Write a program to check if a number entered by the user is greater than 100
+a=int((input("enter a number")))
+if a>100:
+    print("a>100")
+else:
+    print("a<100")
